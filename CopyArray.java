d870eff72e708c6bc467387cc1122aedff38ee6e
Vaaruni S <vaarunis20@gmail.com>
2025-11-29 22:35:10 +0530

    CopyArray.java

---
+++ CopyArray.java
@@ -0,0 +1,15 @@
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        System.out.println("Copied array:");
+        for (int x : arr2) {
+            System.out.println(x);
+        }
+    }
+}
